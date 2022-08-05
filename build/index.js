a99da54822b80763d74c5669f47dda54784a9f03
Devin <once9125@gmail.com>
2022-08-05 23:36:11 +0800

    update git ignore build folder and files

--- build/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
-console.log(process.env.PROJECT_NAME);
-console.log(process.env.MAINTAINER);
-console.log(process.env.NODE_ENV);
