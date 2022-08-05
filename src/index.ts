cb1d71e60bb77d51778b1628e9a9b178ab6009d9
Devin <once9125@gmail.com>
2022-08-05 23:29:30 +0800

    update environment example file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
 
+dotenv.config({ path: `.env.example` });
 console.log(process.env.PROJECT_NAME);
 console.log(process.env.MAINTAINER);
 console.log(process.env.NODE_ENV);
