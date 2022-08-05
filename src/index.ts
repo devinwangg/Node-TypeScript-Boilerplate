7086896aa17a1a42f1f2b73f227d6c0abd72d648
Devin <once9125@gmail.com>
2022-08-05 23:45:10 +0800

    update get environment method

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
 import dotenv from 'dotenv';
 
-dotenv.config({ path: `.env.example` });
-console.log(process.env.PROJECT_NAME);
-console.log(process.env.MAINTAINER);
-console.log(process.env.NODE_ENV);
+enum EnvironmentVersion {
+    example = `.env.example`,
+    development = `.env.dev`,
+    test = `.env.test`,
+    production = `.env.production`
+}
+
+const getEnvVariable = (version: string) => {
+    dotenv.config({ path: version });
+    return {
+        projectName: process.env.PROJECT_NAME,
+        maintainer: process.env.MAINTAINER,
+        envVersion: process.env.NODE_ENV
+    };
+};
+
+const { projectName, maintainer, envVersion } = getEnvVariable(EnvironmentVersion.example);
+console.log(projectName);
+console.log(maintainer);
+console.log(envVersion);
